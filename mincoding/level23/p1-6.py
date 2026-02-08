f7ee9bf0cd42c3fd9d6ba784cb2470349abc1305
young010514 <young010514@naver.com>
2026-02-08 13:47:05 +0900

    260208

--- mincoding/level23/p1-6.py
+++ mincoding/level23/p1-6.py
@@ -1,16 +1,16 @@
-lst = list(input())
+lst = list(map(int,list(input())))
 cnt = 0
-N = input()
+# N = input()
 
 def abc(level,st):
     global cnt
     if level == 4:
-        if len(st) == 4: 
+        if len(str(st)) == 4: 
             cnt += 1
         return
     for i in range(len(lst)):
-        if st and abs(int(st[-1]) - int(lst[i])) <=3:
-            abc(level+1, st + lst[i])
-        elif st == "": abc(level + 1, st + lst[i])
-abc(0,"")
+        if level == 0: abc(level+1, st + lst[i])
+        elif -3<= st%10 - lst[i] <=3:
+            abc(level+1, 10 * st + lst[i])
+abc(0,0)
 print(cnt)
