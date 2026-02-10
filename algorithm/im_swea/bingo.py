b7e80740afa2edacd8f60dcee5bf41cb6b21ffc7
young010514 <young010514@naver.com>
2026-02-10 20:24:55 +0900

    260210-2

--- algorithm/im_swea/bingo.py
+++ algorithm/im_swea/bingo.py
@@ -1,5 +1,3 @@
-import sys
-sys.stdin = open("input_bingo.txt","r")
 
 
 lst = [list(map(int,input().split())) for _ in range(5)]
@@ -8,21 +6,33 @@ check = [[0] * 5 for _ in range(5)]
 call = []
 for _ in range(5):
     call += list(map(int,input().split()))
-bingo = 0
 
-for idx, i in enumerate(call):
-    if bingo == 3 :
-        print(idx + 1)
-        break
-    bingo = 0
+bingo = 0
+idx = 0
+while bingo < 3:
+    i = call[idx]
     for x,inner in enumerate(lst):
         if i not in inner:continue
-        check[x][i.index(inner)] = 1
-
+        check[x][inner.index(i)] = 1
+    bingo = 0
     # 가로줄 확인
     for inner in check :
         if sum(inner) == 5 :bingo +=1
+
     for j in range(5):
-        data = 0
+        data = True
         for i in range(5):
-            data += check
+            if check[i][j] == 0:
+                data = False
+                break
+        if data :
+            bingo += 1
+    right, left = True, True
+    for i in range(5):
+        if check[i][i] == 0 : 
+            right = False
+        if check[i][4-i] == 0: 
+            left = False
+    bingo += (right + left)
+    idx += 1
+print(idx)
