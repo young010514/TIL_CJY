f36b83c70a3b91ee514023f492b2b8cb2616bb2c
young010514 <young010514@naver.com>
2026-02-17 20:26:51 +0900

    260217

--- algorithm/a_practice/region_devide.py
+++ algorithm/a_practice/region_devide.py
@@ -8,22 +8,11 @@ for tc in range(1,T+1):
     n = int(input())
     arr = [list(map(int,input().split())) for _ in range(n) ]
     person = list(map(int,input().split()))
-    rawsum = sum(person)
-    used = [0] * n
-    Min = rawsum
-    st = 0
+    
+    def bfs():
+        return
 
 
-    q =deque()
-    used[0] = 1
-    q.append([0,person[0]])
-    while q:
-        now, nowsum = q.popleft()
-        if Min > abs(rawsum - 2*nowsum):
-            Min = abs(rawsum -2 * nowsum)
-        for i in range(n):
-            if arr[now][i] == 1 and used[i] ==0 :
-                used[i] = 1
-                q.append([i,nowsum + person[i]])
 
-    print(f"#{tc} {Min}")
+
+    print(f"#{tc} ")
