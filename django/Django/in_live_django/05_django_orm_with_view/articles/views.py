0e585d06dcf5f057f26d8128ae661cac5fd18c83
young010514 <young010514@naver.com>
2026-04-15 17:55:33 +0900

    260415

--- django/Django/in_live_django/05_django_orm_with_view/articles/views.py
+++ django/Django/in_live_django/05_django_orm_with_view/articles/views.py
@@ -21,27 +21,27 @@ def new(request):
     return render(request, 'articles/new.html')
 
 
-# def create(request):
-#     # title = request.GET.get('title') 
-#     # content = request.GET.get('content')
-#     title = request.POST.get('title')       # GET에서 POST로 요청 변경
-#     content = request.POST.get('content')   # GET에서 POST로 요청 변경
-#     # 1. 인스턴스 생성 후 속성 할당 및 저장
-#     # article = Article()
-#     # article.title = title
-#     # article.content = content
-#     # article.save()
+def create(request):
+    # title = request.GET.get('title') 
+    # content = request.GET.get('content')
+    title = request.POST.get('title')       # GET에서 POST로 요청 변경
+    content = request.POST.get('content')   # GET에서 POST로 요청 변경
+    # 1. 인스턴스 생성 후 속성 할당 및 저장
+    # article = Article()
+    # article.title = title
+    # article.content = content
+    # article.save()
 
-#     # 2. 인스턴스 생성 시 속성 할당 후 저장
-#     article = Article(title=title, content=content)
-#     article.save()
+    # 2. 인스턴스 생성 시 속성 할당 후 저장
+    article = Article(title=title, content=content)
+    article.save()
 
-#     # 3. create() 메서드를 통한 인스턴스 생성 및 즉시 저장
-#     # Article.objects.create(title=title, content=content)
+    # 3. create() 메서드를 통한 인스턴스 생성 및 즉시 저장
+    # Article.objects.create(title=title, content=content)
     
-#     # return render(request, 'articles/create.html')
+    # return render(request, 'articles/create.html')
 
-#     return redirect('articles:detail', article.pk)
+    return redirect('articles:detail', article.pk)
 
 
 # def delete(request, pk):
