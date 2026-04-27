95ee39f5def5c0f78fc5249ad690d8060a403327
young010514 <young010514@naver.com>
2026-04-27 14:49:15 +0900

    260427

---
+++ DB/01_db_sql/01_single_table_queries.sql
@@ -0,0 +1,69 @@
+-- 01. Querying data
+SELECT 
+  LastName
+FROM
+  employees;
+
+
+
+-- 02. Sorting data
+SELECT
+  FirstName
+From
+  employees
+ORDER BY
+  FirstName ASC;
+
+
+
+
+
+
+
+-- NULL 정렬 예시
+SELECT 
+  ReportsTo
+FROM
+  employees
+ORDER BY
+  ReportsTo;
+
+
+-- 03. Filtering data
+SELECT
+  Country
+FROM
+  customers
+ORDER BY
+  Country;
+
+
+
+-- 04. Grouping data
+-- SELECT 
+--   c1, c2,..., cn, aggregate_function(ci)
+-- FROM
+--   table_name
+-- GROUP BY 
+--   c1, c2, ..., cn;
+
+SELECT
+  Country
+FROM
+  customers
+GROUP BY
+  Country;
+
+
+-- 에러
+SELECT
+  Composer,
+  AVG(Milliseconds / 60000) AS avgOfMinute
+FROM
+  tracks
+WHERE 
+  avgOfMinute < 10
+GROUP BY
+  Composer;
+
+-- 에러 해결
