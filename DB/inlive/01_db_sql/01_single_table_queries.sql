cdea23f95da73a442be1dc2cdb61ab6970f5c2ad
young010514 <young010514@naver.com>
2026-04-28 08:37:50 +0900

    260428

--- DB/inlive/01_db_sql/01_single_table_queries.sql
+++ DB/inlive/01_db_sql/01_single_table_queries.sql
@@ -1,29 +1,54 @@
 -- 01. Querying data
 SELECT 
-  LastName
-FROM
+  LastName 
+FROM 
   employees;
 
+SELECT 
+  LastName, FirstName
+FROM 
+  employees;
 
+SELECT 
+  *
+FROM 
+  employees;
+
+SELECT 
+  FirstName AS '이름'
+FROM 
+  employees;
+
+SELECT
+  Name,
+  Milliseconds / 60000 AS '재생 시간(분)'
+FROM
+  tracks;
 
 -- 02. Sorting data
-SELECT
+SELECT 
   FirstName
-From
+FROM 
   employees
-ORDER BY
-  FirstName ASC;
+ORDER BY 
+  FirstName DESC;
 
 SELECT
-  Country,City
-From
+  Country, City
+FROM 
   customers
 ORDER BY
   Country DESC,
-  City;
+  City ASC;
 
 
-
+SELECT
+  Name,
+  Milliseconds / 60000 AS '재생 시간(분)'
+FROM
+  tracks
+ORDER BY
+  Milliseconds DESC;
 
 
 -- NULL 정렬 예시
@@ -36,13 +61,109 @@ ORDER BY
 
 
 -- 03. Filtering data
-SELECT
+SELECT DISTINCT
   Country
 FROM
   customers
 ORDER BY
   Country;
 
+SELECT
+  LastName, FirstName, City
+FROM
+  customers
+WHERE
+  City != 'Prague';
+
+SELECT
+  LastName, FirstName, Company, Country
+FROM
+  customers
+WHERE 
+-- NULL = NULL 결과: NULL
+-- 값이 없는 것 = 값이 없는 것 ==> 둘 다 모르는 상태에서 빅하는거라 결과도 몰라.
+-- "3치(진) 논리"
+  Company IS NULL 
+  AND Country = 'USA';
+  
+
+SELECT
+  LastName, FirstName, Company, Country
+FROM
+  customers
+WHERE 
+  Company IS NULL 
+  OR Country = 'USA';
+
+
+SELECT
+  Name, Bytes
+FROM
+  tracks
+WHERE
+  Bytes BETWEEN 10000 AND 500000;
+-- WHERE
+--   Bytes >= 10000
+--   AND Bytes <= 500000;
+
+
+SELECT
+  Name, Bytes
+FROM
+  tracks
+WHERE
+  Bytes BETWEEN 10000 AND 500000
+ORDER BY
+  Bytes ASC;
+
+SELECT
+  LastName, FirstName, Country
+FROM
+  customers
+WHERE
+  Country IN ('Canada', 'France', 'Germany');
+
+SELECT
+  LastName, FirstName, Country
+FROM
+  customers
+WHERE
+  Country NOT IN ('Canada', 'France', 'Germany');
+
+
+SELECT
+  LastName, FirstName
+FROM
+  customers
+WHERE
+  LastName LIKE '%son';
+
+
+SELECT
+  LastName, FirstName
+FROM
+  customers
+WHERE
+  LastName LIKE '___a';
+
+
+SELECT
+  TrackId, Name, Bytes
+FROM
+  tracks
+ORDER BY
+  Bytes DESC
+LIMIT 7;
+
+
+SELECT
+  TrackId, Name, Bytes
+FROM
+  tracks
+ORDER BY
+  Bytes DESC
+-- LIMIT 3, 4
+LIMIT 4 OFFSET 3;
 
 
 -- 04. Grouping data
@@ -60,6 +181,19 @@ FROM
 GROUP BY
   Country;
 
+SELECT
+  Composer,
+  AVG(Bytes)
+FROM 
+  tracks
+GROUP BY
+  Composer
+ORDER BY
+  AVG(Bytes) DESC;
+
+
+
+
 
 -- 에러
 SELECT
@@ -73,3 +207,12 @@ GROUP BY
   Composer;
 
 -- 에러 해결
+SELECT
+  Composer,
+  AVG(Milliseconds / 60000) AS avgOfMinute
+FROM
+  tracks
+GROUP BY
+  Composer
+HAVING
+  avgOfMinute < 10;
