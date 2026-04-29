87941efe56faede3a6e1c0057f507c12c3ecc092
young010514 <young010514@naver.com>
2026-04-30 08:18:25 +0900

    260430

--- DB/inlive/03_01_db_many_to_one_relationships/articles/urls.py
+++ DB/inlive/03_01_db_many_to_one_relationships/articles/urls.py
@@ -8,4 +8,12 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('<int:article_id>/delete/', views.delete, name='delete'),
     path('<int:article_id>/update/', views.update, name='update'),
+    # 댓글 create
+    path('<int:article_id>/comments/', views.comments_create, name='comments_create'),
+    # 댓글 delete
+    path(
+        '<int:article_id>/comments/<int:comment_id>/delete/', 
+        views.comments_delete, 
+        name='comments_delete'
+    ),
 ]
