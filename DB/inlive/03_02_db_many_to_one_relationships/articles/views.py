1d7cf974213379ff378f035267092cf2550b9637
young010514 <young010514@naver.com>
2026-05-04 17:16:46 +0900

    260504

--- DB/inlive/03_02_db_many_to_one_relationships/articles/views.py
+++ DB/inlive/03_02_db_many_to_one_relationships/articles/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.views.decorators.http import require_http_methods, require_safe, require_POST
 
 from .forms import ArticleForm, CommentForm
 from .models import Article, Comment
 
 
+@require_safe
 def index(request):
     articles = Article.objects.all()
     context = {
@@ -27,7 +29,7 @@ def create(request):
     }
     return render(request, 'articles/create.html', context)
 
-
+@require_safe
 def detail(request, article_id):
     article = Article.objects.get(pk=article_id)
     comment_form = CommentForm()
