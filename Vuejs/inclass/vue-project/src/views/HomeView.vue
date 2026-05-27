3a08968368df222263b77571d3bf3e1bfe75da25
young010514 <young010514@naver.com>
2026-05-27 13:36:52 +0900

    260527

---
+++ Vuejs/inclass/vue-project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+import { ref } from "vue";
+const message = ref("Hello Vue");
+
+</script>
+
+<template>
+  <!-- <h1>Home</h1> -->
+  <div>message : {{ message }}</div>
+  
+
+
+</template>
+
